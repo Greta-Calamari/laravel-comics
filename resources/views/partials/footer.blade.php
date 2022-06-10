a69964d8153807cfd7ada8db67a1a5a110a89944
ladykakki <greta_calamari@hotmail.it>
2022-06-10 16:17:24 +0200

    foglio stile non si collega

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,119 @@
+<div class="footer">
+
+        <div class="footer-container">
+        <div class="lists-container">
+                <div class="ul-cont">
+        <ul>
+        <h4 class="titleFooter">DC COMICS</h4>
+            
+                <li class="sub"> <a href="/characters">Characters</a> </li>
+                <li class="sub"> <a href="/comics">Comics</a></li>
+                <li class="sub"> <a href="/movies">Movies</a></li>
+                <li class="sub"> <a href="/tv">Tv</a></li>
+                <li class="sub"> <a href="/games">Games</a></li>
+                <li class="sub"> <a href="/collectibles">Collectibles</a></li>
+                <li class="sub"> <a href="/videos">Videos</a></li>
+                <li class="sub"> <a href="/fans">Fans</a></li>
+                <li class="sub"> <a href="/news">News</a></li>
+                <li class="sub"> <a href="/shop">Shop</a></li>
+        </ul>
+
+        
+        <ul>
+        <h4 class="titleFooter">DC</h4>
+            <li class="sub"> <a href="/characters">Characters</a> </li>
+                <li class="sub"> <a href="/comics">Comics</a></li>
+                <li class="sub"> <a href="/movies">Movies</a></li>
+                <li class="sub"> <a href="/tv">Tv</a></li>
+                <li class="sub"> <a href="/games">Games</a></li>
+                <li class="sub"> <a href="/collectibles">Collectibles</a></li>
+                <li class="sub"> <a href="/videos">Videos</a></li>
+                <li class="sub"> <a href="/fans">Fans</a></li>
+                <li class="sub"> <a href="/news">News</a></li>
+                <li class="sub"> <a href="/shop">Shop</a></li>
+        </ul>
+
+        
+        <ul>
+        <h4 class="titleFooter">SITES</h4>
+            <li class="sub"> <a href="/characters">Characters</a> </li>
+                <li class="sub"> <a href="/comics">Comics</a></li>
+                <li class="sub"> <a href="/movies">Movies</a></li>
+                <li class="sub"> <a href="/tv">Tv</a></li>
+                <li class="sub"> <a href="/games">Games</a></li>
+                <li class="sub"> <a href="/collectibles">Collectibles</a></li>
+                <li class="sub"> <a href="/videos">Videos</a></li>
+                <li class="sub"> <a href="/fans">Fans</a></li>
+                <li class="sub"> <a href="/news">News</a></li>
+                <li class="sub"> <a href="/shop">Shop</a></li>
+        </ul>
+
+        </div>
+
+        <div class="ul-cont-2">
+            <ul>
+        <h4 class="titleFooter">SHOP</h4>
+            <li class="sub"> <a href="/characters">Characters</a> </li>
+                <li class="sub"> <a href="/comics">Comics</a></li>
+                <li class="sub"> <a href="/movies">Movies</a></li>
+                <li class="sub"> <a href="/tv">Tv</a></li>
+                <li class="sub"> <a href="/games">Games</a></li>
+                <li class="sub"> <a href="/collectibles">Collectibles</a></li>
+                <li class="sub"> <a href="/videos">Videos</a></li>
+                <li class="sub"> <a href="/fans">Fans</a></li>
+                <li class="sub"> <a href="/news">News</a></li>
+                <li class="sub"> <a href="/shop">Shop</a></li>
+        </ul>
+            
+
+
+        </div>
+
+        </div>
+        </div>
+
+        <div class="footer2sec">
+            <div class="container-subFooter">
+                <button class="button">
+                    <p>SIGN-UP NOW !</p>
+
+
+                </button>
+
+                <div>
+                    <ul class="ul-footer">
+
+                        <li class="img-as">
+                        <h3 class="imp">FOLLOW US</h3>
+                        </li>
+                        <li class="img-as">
+                            <img src="../img/footer-facebook.png" alt="">
+                        </li>
+                        <li class="img-as">
+                            <img src="../img/footer-twitter.png" alt="">
+
+                        </li>
+                        <li class="img-as">
+                            <img src="../img/footer-youtube.png" alt="">
+
+                        </li>
+                        <li class="img-as">
+                            <img src="../img/footer-pinterest.png" alt="">
+
+                        </li>
+                        <li class="img-as">
+                            <img src="../img/footer-periscope.png" alt="">
+
+                        </li>
+
+
+                    </ul>
+                </div>
+
+
+
+            </div>
+
+        </div>
+</div>
+ 
