1bb3a8725e20dad3a58f1428a48d4b132d22b37c
ladykakki <greta_calamari@hotmail.it>
2022-06-10 17:50:38 +0200

    pagina comics costruzione

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -11,6 +11,22 @@ DC COMICS - Comics
 <div class="button-second">
 </div>
 <div class="banner"></div>
+<div class="container">
+    <div class="row">
+        <div class="col-1">
+            <h2>ACTION COMICS #1000 THE DELUXE EDITION</h2>
+            <div class="avaible"></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam alias vero hic non iure? Quis, eveniet quod sunt facilis ex debitis earum adipisci asperiores minus beatae repellendus autem tenetur ratione reiciendis sequi voluptate vel expedita aliquam vitae numquam. Neque excepturi corrupti illo saepe deleniti odio rem cum quisquam iste cupiditate.</p>
+
+        </div>
+        <div class="col-2">
+            <p class="adv">Advertisement</p>
+            <img src="../img/adv.jpg" alt="">
+
+        </div>
+    </div>
+    
+</div>
     
 
 
