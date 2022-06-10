145af4d19fcb7aef2c3736d678109c3f556e1e52
ladykakki <greta_calamari@hotmail.it>
2022-06-10 15:10:25 +0200

    collegato con route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('characters');
+});
+Route::get('/collectibles', function () {
+    return view('collectibles');
 });
